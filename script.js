b7a8021fbf3c6a8c34c461192062a8b36b89a1f7
Nikita Konovalov <mipo4t@gmail.com>
2019-12-15 23:47:30 +0400

    add minute hand moving

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-window.addEventListener('DOMContentLoaded', main);
+window.addEventListener("DOMContentLoaded", main);
 
 const SELECTORS = {
-  CLOCK: '#clock',
-  MINUTE_HAND: '#minute-hand'
+  CLOCK: "#clock",
+  MINUTE_HAND: "#minute-hand"
 };
 
 const STATES = {
-  AUTOMATIC: 'automatic',
-  MANIPULATING_RUN: 'manipulating run',
-  MANIPULATING_STOP: 'manipulating stop'
+  AUTOMATIC: "automatic",
+  MANIPULATING_RUN: "manipulating run",
+  MANIPULATING_STOP: "manipulating stop"
 };
-
 
 function main() {
   app.init();
@@ -24,7 +23,7 @@ const app = {
   },
   currentState: null,
   setCurrentState: function(state) {
-    console.log('SET', state);
+    console.log("SET", state);
     switch (state) {
       case STATES.AUTOMATIC:
         this.currentState = state;
@@ -37,7 +36,7 @@ const app = {
           this.clock.setGlobalTimePeriodically.stop();
         }
 
-        document.addEventListener('mousemove', this.minuteHand.onMouseMove);
+        document.addEventListener("mousemove", this.minuteHand.onMouseMove);
         document.onmouseup = () =>
           this.setCurrentState(STATES.MANIPULATING_STOP);
 
@@ -50,7 +49,10 @@ const app = {
         }
 
         if (this.currentState === STATES.MANIPULATING_RUN) {
-          document.removeEventListener("mousemove", this.minuteHand.onMouseMove);
+          document.removeEventListener(
+            "mousemove",
+            this.minuteHand.onMouseMove
+          );
           document.onmouseup = null;
         }
 
@@ -65,23 +67,38 @@ const app = {
     init: function() {
       this.element = document.querySelector(SELECTORS.CLOCK);
       this.domRect = this.element.getBoundingClientRect();
-      this.setGlobalTimePeriodically = getPeriodicProcess(() => this.setGlobalTime(), 1000);
+      this.center = getClientRectCenter(this.domRect);
+      this.setGlobalTimePeriodically = getPeriodicProcess(
+        () => this.setGlobalTime(),
+        1000
+      );
       this.checkIfXYInside = getCheckIfCoordsInBoundingClientRect(this.domRect);
     },
     element: null,
     domRect: null,
+    center: {
+      x: 0,
+      y: 0
+    },
+    currentTime: {
+      hours: 0,
+      minutes: 0
+    },
     setClockCustomProperty: function(name, value) {
       this.element && this.element.style.setProperty(`--${name}`, value);
     },
-    setClockTime: function(time) {
-      this.setClockCustomProperty('hours', time.getHours());
-      this.setClockCustomProperty('minutes', time.getMinutes());
+    setClockTime: function(hours, minutes) {
+      this.currentTime.hours = hours;
+      this.currentTime.minutes = minutes;
+      this.setClockCustomProperty("hours", hours);
+      this.setClockCustomProperty("minutes", minutes);
     },
     setGlobalTime: function() {
-      this.setClockTime(new Date());
+      const time = new Date();
+      this.setClockTime(time.getHours(), time.getMinutes());
     },
     setGlobalTimePeriodically: null,
-    checkIfXYInside: null,
+    checkIfXYInside: null
   },
   minuteHand: {
     init: function() {
@@ -89,22 +106,45 @@ const app = {
       this.element.ondragstart = () => false;
       this.element.onmousedown = this.onMouseDownHandler;
     },
+    element: null,
+    currentCoords: {
+      x: 0,
+      y: 0
+    },
+    setCurrentCoords: function(x, y) {
+      app.minuteHand.currentCoords.x = x;
+      app.minuteHand.currentCoords.y = y;
+    },
     onMouseDownHandler: function(e) {
       e.preventDefault();
       e.stopPropagation();
       app.setCurrentState(STATES.MANIPULATING_RUN);
       const { clientX, clientY } = e;
-      console.log('START', clientX, clientY);
+      app.minuteHand.setCurrentCoords(clientX, clientY);
     },
-    element: null,
     onMouseMove: function(e) {
       const { clientX, clientY } = e;
       if (!app.clock.checkIfXYInside(clientX, clientY)) {
         app.setCurrentState(STATES.MANIPULATING_STOP);
         return;
       }
-      console.log('NEW', clientX, clientY);
-    },
+
+      const angle = getAngleInRads(
+        app.clock.center,
+        app.minuteHand.currentCoords,
+        { x: clientX, y: clientY }
+      );
+
+      const minutes = convertRadsToMinutes(angle);
+      
+      if (!minutes) {
+        return;
+      }
+
+      const newTime = addMinutesToTime(app.clock.currentTime, minutes);
+      app.minuteHand.setCurrentCoords(clientX, clientY);
+      app.clock.setClockTime(newTime.hours, newTime.minutes);
+    }
   }
 };
 
@@ -130,4 +170,57 @@ function getPeriodicProcess(fn, period = 1000) {
 function getCheckIfCoordsInBoundingClientRect(rect) {
   return (x, y) =>
     x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom;
+}
+
+function getClientRectCenter(rect) {
+  const x = Math.round((rect.right - rect.left) / 2) + rect.left;
+  const y = Math.round((rect.bottom - rect.top) / 2) + rect.top;
+  return { x, y };
+}
+
+function getAngleInRads(center, prev, next) {
+  // equation of line by 2 points: x(y0 - y1) + y(x1 - x0) + (x0y1 - y0x1) = 0
+  //
+  // 2 equation of line: xa1 + yb1 + c1 = 0
+  //                     xa2 + yb2 + c2 = 0
+  //
+  // cos(angle) = (a1*a2 + b1*b2) / (sqrt(a1^2 + b1^2) * sqrt(a2^2 + b2^2))
+
+  const a1 = center.y - prev.y;
+  const b1 = prev.x - center.x;
+
+  const a2 = center.y - next.y;
+  const b2 = next.x - center.x;
+
+  const cosAngle =
+    (a1 * a2 + b1 * b2) /
+    (Math.sqrt(a1 * a1 + b1 * b1) * Math.sqrt(a2 * a2 + b2 * b2));
+
+    return Math.acos(cosAngle);
+}
+
+function convertRadsToMinutes(radian) {
+  // 1min = 6deg
+  // angleInDegs = angleInRads * 180 / pi;
+  // angleInMinutes = angleInRads * 180 / pi / 6 = angleInReds * 30 / pi;
+
+  return (radian * 30) / Math.PI;
+}
+
+function addMinutesToTime(time, minutes) {
+  const newMinutes = time.minutes + minutes;
+  if (newMinutes < 60) {
+    return { ...time, minutes: newMinutes };
+  }
+
+  const correctedMinutes = newMinutes - 60;
+  const newHours = time.hours + 1;
+
+  if (newHours < 24) {
+    return { hours: newHours, minutes: correctedMinutes };
+  }
+
+  const correctedHours = newHours - 24;
+
+  return { hours: correctedHours, minutes: correctedMinutes };
 }
